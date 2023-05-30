1761fcb717691db0ac473cbdbcc5d8b95dd76787
Artem <72685480+magafurov-artem@users.noreply.github.com>
2023-05-30 18:59:03 +0500

    Transfer to MySql

--- config/settings.py
+++ config/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': getenv('MYSQL-USER'),
+        'PASSWORD': getenv('MYSQL-PASSWORD'),
+        'HOST': getenv('MYSQL-HOST'),
+        'PORT': getenv('MYSQL-PORT'),
+        'NAME': getenv('MYSQL-SCHEMA')
     }
 }
 
