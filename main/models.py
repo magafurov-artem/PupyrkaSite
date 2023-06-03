43af37ee32723d0f7795915b457a425a91a78a67
Artem <72685480+magafurov-artem@users.noreply.github.com>
2023-06-03 19:42:25 +0500

    Дополнение структуры бд

--- main/models.py
+++ main/models.py
@@ -3,5 +3,19 @@ from django.db import models
 
 class Specialties(models.Model):
     title = models.CharField(max_length=255)
+    speciality = models.ForeignKey('SpecialtiesIds', on_delete=models.PROTECT)
     about = models.TextField()
     image_preview_url = models.CharField(max_length=255)
+
+
+class Admissions(models.Model):
+    speciality = models.ForeignKey('SpecialtiesIds', on_delete=models.PROTECT)
+    fio = models.CharField(max_length=32, verbose_name='Ф.И.О.')
+    birthday = models.DateField(verbose_name='День рождения')
+    education_status = models.CharField(max_length=16, verbose_name='Ваше текущее образование')
+    phone = models.CharField(max_length=32, verbose_name='Номер телефона')
+    email = models.CharField(max_length=32, verbose_name='Электронная почта')
+
+
+class SpecialtiesIds(models.Model):
+    name = models.CharField(max_length=16, verbose_name='Специальность')
